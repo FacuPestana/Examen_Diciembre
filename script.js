b7acba7dec8b524253d03b30fccb41cf0e221fd6
alumno <alumno@Instituto.local>
2024-02-16 08:22:29 -0300

    Version 1.2
    Explique lo que hace cada cosa en el codigo

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
+/*1 Auto
+Aca se hace lo mismo a todos, se pone let y se crea la variable;
+document.querySelector("#Negro") para decir que se va a seleccionar un elemento en especifico de
+el archivo HTML, y se pone NEGRO.
+
+Se pone el nombre de la variable para indicar que se va a estar utilizando,se pone un punto y 
+al hace que se va a ejecutar esa linea de codigo.
+
+Pones function para indicar que se va a crear una nueva Funcion y los parentesis (que son los paramentros 
+pero como no hay parametros se ponen igual y no se pone nada adentro).
+
+Despues se indica en "foto.src", que va a ser a la foto que va a cambiar, pones igual y a la foto que tengas ya sea descargada o que la buscas en la web copiando el link.
+
+Tambien esta Texto.innerText = Negro Vulcano, que va a ser al texto que va a combiar cuando vos en este caso pases el mouse por arriba de el boton
+(pero tranquilamente puede ser al hacer click).
+
+Function nombre(Parametros){
+    nombredelaVariable.src = (src de la foto a la que queres cambiar)
+    Texto.innerText = (Al texto que va a cambiar cuando se ejecute el evento que dice, en este caso OnMouseOver)
+}
+*/
+
+
 let botonNegro = document.querySelector("#Negro")
-botonNegro.onclick =
+botonNegro.onmouseover =
 function (){
     foto.src = "cronosNegro.png"
     Texto.innerText = "NEGRO VULCANO"
 } 
 let botonBlanco = document.querySelector("#Blanco")
-botonBlanco.onclick =
+botonBlanco.onmouseover =
 function (){
     foto.src = "cronosBlanco.png"
     Texto.innerText = "FIAT BLANCO"
 
 } 
 let botonRojo =  document.querySelector("#Rojo")
-botonRojo.onclick =
+botonRojo.onmouseover =
 function (){
     foto.src = "cronosRojo.png"
     Texto.innerText = "FIAT ROJO"
